fd8234643cafa50f7f4c1c54b7713a342f9bfa78
Askold Astakhov <astakhovaskold@gmail.com>
2025-02-10 10:34:27 +0300

    add error boundary v2

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,4 +1,3 @@
-import {Spin} from 'antd';
 import {memo, Suspense} from 'react';
 import {createRoutesFromElements, Route, RouterProvider} from 'react-router';
 
@@ -8,54 +7,47 @@ import App from '../App';
 
 import {RouteGuard} from './RouteGuard';
 
+import SuspenseLoader from '@/app/components/Presentation/SuspenseLoader';
 import ErrorBoundary from '@/app/components/Utils/ErrorBoundary';
 import {routes} from '@/router/routes';
 
 const Navigation = memo(() => {
     return (
-        <Suspense fallback={<>Loading...</>}>
-            <RouterProvider
-                router={createBrowserRouter(
-                    createRoutesFromElements(
-                        <Route
-                            key="parent"
-                            path="/"
-                            element={<App />}
-                            errorElement={<ErrorBoundary />}
-                            hasErrorBoundary
-                        >
-                            {routes.map(({path, title, component: Component, ...guardProps}) => {
-                                const {restrictedWithAuth, isPublic, roles} = guardProps;
+        <RouterProvider
+            router={createBrowserRouter(
+                createRoutesFromElements(
+                    <Route key="parent" path="/" element={<App />} errorElement={<ErrorBoundary />} hasErrorBoundary>
+                        {routes.map(({path, title, component: Component, ...guardProps}) => {
+                            const {restrictedWithAuth, isPublic, roles} = guardProps;
 
-                                const key = path === '/' ? path : `/${path}`;
+                            const key = path === '/' ? path : `/${path}`;
 
-                                const isProtected = !isPublic || restrictedWithAuth || roles;
+                            const isProtected = !isPublic || restrictedWithAuth || roles;
 
-                                const children = (
-                                    <Route
-                                        key={key}
-                                        path={key}
-                                        element={
-                                            <Suspense fallback={<Spin spinning />}>
-                                                <Component />
-                                            </Suspense>
-                                        }
-                                    />
-                                );
+                            const children = (
+                                <Route
+                                    key={key}
+                                    path={key}
+                                    element={
+                                        <Suspense fallback={<SuspenseLoader />}>
+                                            <Component />
+                                        </Suspense>
+                                    }
+                                />
+                            );
 
-                                if (!isProtected) return children;
+                            if (!isProtected) return children;
 
-                                return (
-                                    <Route key={key} element={<RouteGuard title={title} {...guardProps} />}>
-                                        {children}
-                                    </Route>
-                                );
-                            })}
-                        </Route>,
-                    ),
-                )}
-            />
-        </Suspense>
+                            return (
+                                <Route key={key} element={<RouteGuard title={title} {...guardProps} />}>
+                                    {children}
+                                </Route>
+                            );
+                        })}
+                    </Route>,
+                ),
+            )}
+        />
     );
 });
 
